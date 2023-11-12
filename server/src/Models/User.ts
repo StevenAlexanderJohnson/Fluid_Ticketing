d8de14ae8a014326d6f7d4ffc2d16af5b2afc1c0
Steven Johnson <steveajohn1@gmail.com>
2023-11-12 17:51:16 -0500

    fixed build error

--- server/src/Models/User.ts
+++ server/src/Models/User.ts
@@ -10,14 +10,14 @@ export interface UserInterface {
 
 export default class User implements BSON.Document {
     public _id: ObjectId;
-    public name: string | undefined;
+    public name: string;
     public email: string;
     public password: string;
     public role: string;
 
     constructor(data?: UserInterface) {
         this._id = data && data['id'] ? new ObjectId(data['id']) : new ObjectId();
-        this.name = data && data['name'] ? data['name'] : undefined;
+        this.name = data && data['name'] ? data['name'] : "";
         this.email = data && data['email'] ? data['email'] : "";
         this.password = data && data['password'] ? data['password'] : "";
         this.role = data && data['role'] ? data['role'] : "user";
